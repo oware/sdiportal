7cadfcc66b4b797ea857522b21f136597db6a08a
Allan Oware <allanoware@gmail.com>
2017-04-26 17:41:40 +0300

    initial setup

---
+++ README.rst
@@ -0,0 +1,26 @@
+SDI-Portal
+===========
+
+SDI-Portal is a custom GeoNode-2.6 project based on geonode-project.
+
+Installation
+------------
+
+Install geonode with::
+
+    $ sudo add-apt-repository ppa:geonode/stable
+
+    $ sudo apt-get update
+
+    $ sudo apt-get install geonode
+
+Create a new template based on the geonode example project.::
+    
+    $ django-admin startproject sdiportal --template=https://github.com/GeoNode/geonode-project/archive/2.6.zip -epy,rst 
+    $ sudo pip install -e sdiportal
+
+.. note:: You should NOT use the name geonode for your project as it will conflict with the default geonode package name.
+
+Features
+--------
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="sdiportal",
+    version="0.2",
+    author="",
+    author_email="",
+    description="sdiportal, based on GeoNode",
+    long_description=(read('README.rst')),
+    # Full list of classifiers can be found at:
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+    license="BSD",
+    keywords="sdiportal geonode django",
+    url='https://github.com/sdiportal/sdiportal',
+    packages=['sdiportal',],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django-tastypie==0.13.0',
+    ]
+)
